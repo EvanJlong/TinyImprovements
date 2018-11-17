152bcaed3f6a858be73fff21c460e9c1eccfa42d
EJL <EvanJLong11@gmail.com>
2018-11-17 14:44:15 -0600

    fixing mongoose database mlab connection  WORKING NOW

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const keys = require("./config/keys.js");
 const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const db = require('./models')
+const mongodb_URI = require("./config/keys.js")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost/tinyImprovements', { useNewUrlParser: true });
+
+mongoose.connect(mongodb_URI, { useNewUrlParser: true });
+// mongoose.connect(process.env.mongodb_URI || 'mongodb://localhost/tinyImprovements', { useNewUrlParser: true });
 
 require('./routes/api-routes')(app);
 
