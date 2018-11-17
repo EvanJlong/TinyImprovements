417df0da9252eb06f8800a9a4144d0d62f5d1ae4
EJL <EvanJLong11@gmail.com>
2018-11-16 23:53:43 -0600

    all done passing over to heroku and mlab shortly

--- models/Kudos.js
+++ models/Kudos.js
@@ -4,7 +4,10 @@ const Schema = mongoose.Schema;
 //creating a new schema to log the Kudos//
 var KudoSchema = new Schema({
     title: String,
-    message: String
+    body: String,
+    from: String,
+    to: String
+
 })
 
 const Kudo = mongoose.model('Kudo',KudoSchema);
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,7 +4,7 @@ const Kudo = require('../models/Kudos');
 // const db = require('../models');
 
 module.exports = function (app) {
-
+    //user get route//
     app.get('/api/user', function (req, res) {
         User.find({})
             .populate('kudos')
@@ -16,6 +16,7 @@ module.exports = function (app) {
             });
     });
 
+    //kudos get route//
     app.get('/api/kudos', function (req, res) {
         Kudo.find(req.body)
             .then(function (data) {
@@ -25,6 +26,7 @@ module.exports = function (app) {
                 res.json(err);
             });
     });
+
     //kudos update route
     app.put('/api/kudos/:id', function (req, res) {
         Kudo.findOneAndUpdate({ _id: req.params.id }, { $set: req.body })
@@ -36,9 +38,9 @@ module.exports = function (app) {
             });
     });
 
-    //kudos delete route
+    // kudos delete route
     app.delete('/api/kudos/:id', function (req, res) {
-        Kudos.findOneAndDelete({ _id: req.params.id })
+        Kudo.findOneAndDelete({ _id: req.params.id })
             .then(function (data) {
                 res.json(data);
             })
@@ -48,71 +50,33 @@ module.exports = function (app) {
     });
 
 
+    //kudos post route
+    app.post('/api/kudos', function (req, res) {
+        const usersId = req.body.usersId;
+        const newEntry = {
+            title: req.body.title,
+            body: req.body.body,
+            from: req.body.from,
+            to: req.body.to
+        }
+
+        Kudo.create(newEntry)
+            .then(function (kudosData) {
+                return users.findOne({ _id: usersId }, { $push: { kudos: kudosData._id } }, { new: true });
+            })
+            .then(function (usersData) {
+                res.json(usersData);
+            })
+            .catch(function (err) {
+                res.json(err);
+            });
+    });
+
+
+
+
+
+
+
+
 }
-
-
-
-    //user get route
-//   app.get('/api/users', function (req, res) {
-//         db.users.find({})
-//     //     .populate('kudos')
-//         .then(function (data) {
-//           res.json(data);
-//         })
-//         .catch(function (err) {
-//           res.json(err);
-//         });
-//       });
-
-
-// module.exports = function (app) {
-
-//     app.post('/api/session', function (req, res) {
-//       User.find(req.body)
-//       .then(function (data) {
-//         res.json(data);
-//       })
-//       .catch(function (err) {
-//         res.json(err);
-//       });
-//     });
-
-//     app.get('/api/user/:id', function (req, res) {
-//       User.find({_id: req.params.id})
-//       .populate('notes')
-//       .then(function (data) {
-//         res.json(data);
-//       })
-//       .catch(function (err) {
-//         res.json(err);
-//       });
-//     });
-
-//     app.post('/api/user', function (req, res) {
-//       User.create(req.body)
-//       .then(function (data) {
-//         res.json(data);
-//       })
-//       .catch(function (err) {
-//         res.json(err);
-//       });
-//     });
-
-//     app.post('/api/note', function (req, res) {
-//       const userId = req.body.userId;
-//       const newEntry = {
-//         body: req.body.body
-//       }
-
-//       Note.create(newEntry)
-//         .then(function (noteData) {
-//         return User.findOneAndUpdate({_id: userId}, { $push: { notes: noteData._id } }, { new: true });
-//       })
-//       .then(function(userData) {
-//         res.json(userData);
-//       })
-//       .catch(function (err) {
-//         res.json(err);
-//       });
-//     });
-//   }
